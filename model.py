967ba610ef50acc416bdba793718f8da2248229c
Burhaan Amjad Khan <burhaankhanpay786@gmail.com>
2025-07-15 08:21:20 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from PIL import Image
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class BoneNet(nn.Module):
+    def __init__(self):
+        super(BoneNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
+            nn.Conv2d(16, 32, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(32*64*64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 2)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+model = BoneNet().to(device)
+model.load_state_dict(torch.load("best_mura_model.pth", map_location=device))
+model.eval()
+
+def predict(image_path):
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+    ])
+    img = Image.open(image_path).convert("RGB")
+    img = transform(img).unsqueeze(0).to(device)
+    with torch.no_grad():
+        outputs = model(img)
+        _, predicted = torch.max(outputs, 1)
+    return "Abnormal" if predicted.item() == 1 else "Normal"
